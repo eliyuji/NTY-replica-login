4af93163d701f7a593e6480894593cc48854f9c9
Rachel Ng <Rachel@Ralphs-MacBook-Pro.local>
2025-05-10 16:49:33 -0700

    starter code + hw2 + some user authentication development

---
+++ frontend/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [svelte()],
+  test: {
+    globals: true,
+    environment: 'jsdom', 
+    setupFiles: ['./src/setupTests.ts'],
+  },
+  resolve: mode === 'test' ? {
+    conditions: ['browser'],
+  } : {},
+  server: mode === 'development' ? {
+    proxy: {
+      '/api': {
+        target: 'http://backend:8000',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            console.log('Proxying request:', req.url)
+          })
+        }
+      },
+    },
+  } : undefined,
+}))
